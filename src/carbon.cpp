562039bdc84dddc0955fbf65a070b43e37be2161
Ahmed <ahmed.mustafa.talaat@gmail.com>
2026-05-18 04:15:29 +0300

    Check file availability

--- src/carbon.cpp
+++ src/carbon.cpp
@@ -2,6 +2,11 @@
 #include "components/ConfigurationParser.h"
 
 Carbon Carbon::init(const fpath& c14Path) {
+
+    if (!std::filesystem::exists(c14Path)) {
+        throw std::runtime_error("C14 file not found");
+    }
+
     CarbonLab::ConfigurationParser parser(c14Path);
     auto carbon = parser.load();
 
